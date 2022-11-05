c69319b13f61162d7603b967cd67e833fd2e4bc4
bawrdiii <bardiabarbod@live.com>
2022-11-05 11:48:44 +0100

    Working on project

--- pages/addpatient/index.jsx
+++ pages/addpatient/index.jsx
@@ -1,13 +1,23 @@
+import { useRouter } from "next/router"
 import Addpatient from "../../components/add patient/addPatient"
 import Navbar from "../../components/navbar/navbar"
 
 const MainPatient = () => {
 
-    const 
+    const router = useRouter()
+
+    const logOutHandler = () => {
+        const token = localStorage.getItem("Token")
+        if (token) {
+            localStorage.removeItem("Token")
+            router.push("/")
+        }
+        else router.push("/")
+    }
 
     return (
         <>
-            <Navbar logOutHandler={} />
+            <Navbar logOutHandler={logOutHandler} />
             <Addpatient />
         </>
     )
