e6eecb2f98a8f98cbf569433b73f282d55aa3ea8
bawrdiii <bardiabarbod@live.com>
2022-11-24 12:50:07 +0100

    Working on new Signup/Login

--- components/firebaseconfig/firebaseconfig.jsx
+++ components/firebaseconfig/firebaseconfig.jsx
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app"
 import { getFirestore } from "@firebase/firestore"
+import { getAuth } from "firebase/auth"
 
 const firebaseConfig = {
     apiKey: "AIzaSyBmcjL-w72Ixulg2YjmTl3f63yPzi3rlpM",
@@ -14,3 +15,5 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig)
 
 export const db = getFirestore(app)
+
+export const auth = getAuth(app)
